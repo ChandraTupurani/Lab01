dc2d9f64118f43e9faab49f6a85b6e82947c23dc
ChandraTupurani <98793524+ChandraTupurani@users.noreply.github.com>
2022-01-31 23:28:32 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,19 @@
+/************************
+*Hello
+************************
+*Chandra Tupurani
+*1/19/22
+*255 and c90
+******************************/
+ 
+ public class hello {
+     public static void main(String[] args) {
+         
+         System.out.println("<Chandra Tupurani>");
+         System.out.println("<Hello>");
+         System.out.println("<This program says my name, hello, date, and section>");
+         System.out.println("<January 27>");
+         System.out.println("<255 and c90>");
+         System.out.println();
+     }
+ }
